26bfd4520596a24b7b6e895cac6ce6f6aa54805e
Chaoyang Zhu <Chaoyang Zhu>
2022-10-30 19:13:51 -0500

    react

---
+++ src/main/resources/static/precache-manifest.afdd7bf589d0698cdb6cf1753c254954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e33788a5513c0e12b11226130a21223",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "690bdec3b5ce84a456cf",
+    "url": "/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "690bdec3b5ce84a456cf",
+    "url": "/static/js/main.ecde69c8.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
